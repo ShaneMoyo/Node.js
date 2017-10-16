ea354bf0dff5e2a33e969b0bb69b5c988b608f30
Shane Moyo <smoyo@pdx.edu>
2017-10-16 08:40:39 -0700

    All done!

--- requestHandler.js
+++ requestHandler.js
@@ -1,6 +1,7 @@
 var exec = require('child_process').exec; 
 var querystring = require('querystring'),
-    fs = require("fs");
+    fs = require("fs"),
+    formidable = require("formidable");
 
 
 function start(response){
@@ -26,12 +27,27 @@ function start(response){
 }
 
 
-function upload(response, postData){
+function upload(response, request){
     console.log('Request handler upload was called');
-    response.writeHead(200, {"content-type": "text/plain"});
-    response.write("You've sent: +" 
-    + querystring.parse(postData).text);
-    response.end();
+
+    var form = new formidable.IncomingForm(); 
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files){
+        console.log("parsing done");
+
+
+        fs.rename(files.upload.path, "/tmp/test.png", function(error){
+            if (error) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        response.writeHead(200, {"content-type": "text/html"});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
+    
 }
 
 function show(response) {

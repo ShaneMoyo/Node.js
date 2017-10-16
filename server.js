b612d419cf493b4aafc3c378102b098634ae396c
Shane Moyo <smoyo@pdx.edu>
2017-10-15 18:15:59 -0700

    added routeHandler

--- server.js
+++ server.js
@@ -2,20 +2,17 @@ var http = require('http');
 var url = require('url');
 
 
-function start(route){
+function start(route, handle){
 function onRequest(request, response){
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received');
 
-    route(pathname);
-    
-    response.writeHead(200, {"content-type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+    route(handle, pathname);
 }
+   
+
 
 http.createServer(onRequest).listen(8888);
-
 console.log('server has started.');
 }
 
